7e82117b4b81edae70ab4d2d037497d11b3f2d44
KayleeMcLaren <134863501+KayleeMcLaren@users.noreply.github.com>
2023-09-20 22:55:19 +0200

    Update question_model.py

--- 13.QuizGame/question_model.py
+++ 13.QuizGame/question_model.py
@@ -1,5 +1,6 @@
+# Defines a new class called Question
 class Question:
-
+    #  The text attribute stores the text of the question, and the answer attribute stores the correct answer to the question
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
